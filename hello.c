47b12f5d85a03a137eee02cc61e99c917c8ea299
Caleb Paden <cpaden2@huskers.unl.edu>
2022-08-25 18:00:09 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+ /**
+ * Author: Caleb Paden
+ *         cpaden2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Caleb Paden\n");
+
+  return 0;
+}
